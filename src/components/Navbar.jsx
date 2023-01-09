be44a599042ae33ca3a8394b87870f50b7a0d30c
NemanjaGojkovic <n.gojko6@gmail.com>
2023-01-09 21:59:12 +0100

    kreiran navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar(){
+    return (
+    <div class="inner-width">
+        
+        <nav className="navigation-menu">
+          <h1 className='title'>CupCoffee</h1>
+          <a href="#"> Pocetna</a>
+          <a href="#"> Proizvodi</a>
+          <a href="#"> Kontakt</a>
+        </nav>
+      </div>
+
+    )
+}
+
+export default Navbar
